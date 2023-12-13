074aa4d7cf34df367c10b3dba94a3e5b6e97e9ec
GentIeman <ilyashepelev23@gmail.com>
2023-12-13 14:51:28 +0300

    style: delete unused code, added import defineNuxtConfig update: added UI and Eslint modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import {defineNuxtConfig} from "nuxt/config";
+
 export default defineNuxtConfig({
     devtools: {enabled: false},
-    modules: ["@nuxtjs/strapi"],
+    modules: ["@nuxtjs/strapi", "@nuxt/ui", "@nuxtjs/eslint-module"],
     runtimeConfig: {
         strapi: {
             url: 'http://localhost:5555'
-        },
-        public: {
-            strapi: {
-                url: 'http://localhost:5555'
-            }
         }
     }
 })
