36baabdf5141e1e6221f89738dc53c846f2bdcfb
Ilya <76070836+GentIeman@users.noreply.github.com>
2023-12-14 14:31:13 +0300

    Configuration (#2)
    * style: delete unused code, added import defineNuxtConfig
    * update: added UI and Eslint modules
    * feat: added preference color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ import {defineNuxtConfig} from "nuxt/config";
 
 export default defineNuxtConfig({
     devtools: {enabled: false},
-    modules: ["@nuxtjs/strapi", "@nuxt/ui", "@nuxtjs/eslint-module"],
+    modules: ["@nuxtjs/strapi", "@nuxt/ui", "@nuxtjs/eslint-module", "@nuxtjs/color-mode"],
+    colorMode: {
+        preference: "light",
+    },
     runtimeConfig: {
         strapi: {
             url: 'http://localhost:5555'
