851ffefb6a4a4d94feac4e8e9355bcd2ebb0d824
GentIeman <ilyashepelev23@gmail.com>
2023-12-15 16:01:13 +0300

    update: added new settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import {defineNuxtConfig} from "nuxt/config";
+import {defineNuxtConfig} from "nuxt/config"
 
 export default defineNuxtConfig({
     devtools: {enabled: false},
-    modules: ["@nuxtjs/strapi", "@nuxt/ui", "@nuxtjs/eslint-module", "@nuxtjs/color-mode"],
+    modules: ["@nuxtjs/strapi", "@nuxt/ui", "@nuxtjs/eslint-module", "@nuxtjs/color-mode", "@nuxtjs/google-fonts"],
     colorMode: {
         preference: "light",
+    },
+    googleFonts: {
+        display: "swap",
+        download: true,
+        families: {
+            "DM+Sans": [400, 500, 600, 700]
+        }
+    },
+    fontMetrics: {
+        fonts: ["DM Sans"]
     },
     runtimeConfig: {
         strapi: {
