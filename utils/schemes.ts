6713c184c5d3de7b586b2b4078a92bf8316d22a7
GentIeman <ilyashepelev23@gmail.com>
2023-12-16 15:01:04 +0300

    update: changed schema for register form feat: added schema for login form

--- utils/schemes.ts
+++ utils/schemes.ts
@@ -1,9 +1,14 @@
 import {object, string, number} from "yup"
 
-export const formValidationSchema = object({
+export const registerFormValidation = object({
     email: string().email("You must enter an email").required("Required"),
-    username: string().required("Required").min(4, "Must be at least 4 characters"),
-    password: string().required("Required").min(8, "Must be at least 8 characters"),
-    salary: number().required("Required").moreThan(1, "We can't get 0 dollars"),
+    username: string().min(4, "Must be at least 4 characters"),
+    password: string().min(8, "Must be at least 8 characters"),
+    salary: number().moreThan(1, "We can't get 0 dollars"),
     pensionYear: number().transform((value) => Number.isNaN(value) ? new Date().getFullYear() : value ).nullable().min(new Date().getFullYear(), "We can't go back in time").lessThan(new Date().getFullYear()+60, "Too far way")
+})
+
+export const loginFormValidation = object({
+    email: string().email("You must enter an email").required("Required"),
+    password: string().min(8, "Must be at least 8 characters"),
 })
