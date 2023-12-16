e9cfe2fdadb9dd5d1d0d2dbf3e0f1331c3f1c2cd
GentIeman <ilyashepelev23@gmail.com>
2023-12-16 22:59:02 +0300

    feat: function of set the meta information of the header

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,26 @@
 <template>
+  <AppHeader
+    :title="setHeaderMeta(headerMeta, 'title')"
+    :subtitle="setHeaderMeta(headerMeta, 'subtitle')"
+  />
 </template>
 
 <script setup lang="ts">
+const route = useRoute()
+
 definePageMeta({
   middleware: ["auth"]
 })
+
+const headerMeta = [
+  {title: "Dashboard", subtitle: "See your quick overview  data right here", page: "/"}
+]
+const setHeaderMeta = (meta: object, field: string): string => {
+  for (let item of meta) {
+    if (item.page == route.fullPath) return item[field]
+  }
+}
+
 </script>
 
 <style scoped>
