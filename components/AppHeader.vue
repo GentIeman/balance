72d3e7755e8560452622d2a3f962f6ea9926bf81
GentIeman <ilyashepelev23@gmail.com>
2023-12-16 22:56:19 +0300

    init

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="flex flex-col text-zinc-800 text-[40px] font-semibold">
+    {{ title }}
+    <span class="text-zinc-800 text-lg font-light"> {{ subtitle }} </span>
+  </h1>
+</template>
+
+<script setup lang="ts">
+import type {IHeader} from "~/utils/interfaces"
+
+defineProps<IHeader>()
+</script>
+
+<style scoped>
+
+</style>
