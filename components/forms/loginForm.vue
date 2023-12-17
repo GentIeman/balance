d3e667d63ee2198df5b5c0147a4451d68a2b153d
GentIeman <ilyashepelev23@gmail.com>
2023-12-17 23:41:47 +0300

    style: code formatted

--- components/forms/loginForm.vue
+++ components/forms/loginForm.vue
@@ -1,8 +1,8 @@
 <template>
   <UForm
     :state="user"
-    @submit="onAuth('register', $event)"
-    :schema="registerFormValidation"
+    @submit="signIn($event)"
+    :schema="loginFormValidation"
     class="flex flex-col gap-5 h-full"
   >
     <UFormGroup
@@ -15,43 +15,6 @@
         v-model="user.email"
         type="email"
         placeholder="you@example.com"
-      />
-    </UFormGroup>
-    <UFormGroup
-      name="username"
-      size="sm"
-      label="Username"
-      required
-    >
-      <UInput
-        v-model="user.username"
-        type="text"
-        placeholder="username"
-      />
-    </UFormGroup>
-    <UFormGroup
-      name="salary"
-      size="sm"
-      label="Salary"
-      required
-    >
-      <UInput
-        v-model="user.salary"
-        type="number"
-        @change="user.salary = $event.target.value == '' ? 1 : state.salary"
-        placeholder="1000"
-      />
-    </UFormGroup>
-    <UFormGroup
-      name="pensionYear"
-      size="sm"
-      label="Penstion date"
-      hint="Optional"
-    >
-      <UInput
-        v-model="user.pensionYear"
-        type="number"
-        :placeholder="`${currentYear}`"
       />
     </UFormGroup>
     <UFormGroup
@@ -82,33 +45,31 @@
       block
       type="submit"
     >
-      Sign Up
+      Sign In
     </UButton>
   </UForm>
 </template>
 
 <script setup lang="ts">
+import {loginFormValidation} from "~/utils/schemes"
 import {UButton, UForm, UFormGroup, UInput} from "#components"
-import type {InferType} from "yup"
-import {registerFormValidation} from "~/utils/schemes"
 import type {FormSubmitEvent} from "#ui/types"
+import {type InferType} from "yup"
 import {useAuthStore} from "~/store/authStore"
-const authStore = useAuthStore()
-import {defineProps} from "vue"
+import {useRouter} from "vue-router"
 import type {IAuthFormProps, IUser} from "~/utils/interfaces"
+const authStore = useAuthStore()
 const router = useRouter()
-const authUser = useStrapiUser()
 
 const {state} = defineProps<IAuthFormProps>()
 const user = ref<IUser>(state)
 
 const isShowPassword = ref<boolean>(false)
 const togglePasswordIcon = computed(() => isShowPassword.value ? "i-heroicons-eye-20-solid" : "i-heroicons-eye-slash-20-solid")
-const currentYear = new Date().getFullYear()
 
-type registrationSchema = InferType<typeof registerFormValidation>
-const onAuth = async (type: string, event: FormSubmitEvent<registrationSchema>) => {
-  await authStore.auth(type, event.data)
-  if (authUser.value) router.push("/home")
+type loginSchema = InferType<typeof loginFormValidation>
+const signIn = async (event: FormSubmitEvent<loginSchema>) => {
+  await authStore.auth("login", event.data)
+  router.push("/")
 }
 </script>
