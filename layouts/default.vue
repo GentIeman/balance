77c1ac124f2d725a28a37c66a0b8cdea987cdb7e
GentIeman <ilyashepelev23@gmail.com>
2023-12-17 23:49:22 +0300

    feat: added style for container

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="grid grid-cols-[300px_1fr] h-screen bg-gray-100">
     <AppSideBar />
-    <slot />
+    <div class="grid auto-rows-max gap-10 sm:px-[30px] sm:py-[20px]">
+      <slot />
+    </div>
   </div>
 </template>
 
