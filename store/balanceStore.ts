0986f29a1be4d6850151a54b6b788a82e75dc8b8
GentIeman <ilyashepelev23@gmail.com>
2023-12-19 00:14:32 +0300

    feat: added CRUD for categories and expense

--- store/balanceStore.ts
+++ store/balanceStore.ts
@@ -1,43 +1,92 @@
 import {defineStore} from "pinia"
-import type {ICategory} from "~/utils/interfaces"
+import type {ICategory, IExpense} from "~/utils/interfaces"
+import {useAuthStore} from "~/store/authStore"
 
 export const useBalanceStore = defineStore("balanceStore", {
     state: () => ({
-        balance: {},
-        categories: []
+        categories: [],
+        expenses: []
     }),
     getters: {
         getCategories: (state) => (id?: number) => {
             if (id) return state.categories.some((category: ICategory) => category.id == id)
             return state.categories
-        }
+        },
+        getExpenses: (state) => (id?: number) => {
+            if (id) return state.expenses.some((expense: IExpense) => expense.id == id)
+            return state.expenses
+        },
     },
     actions: {
         async fetchUserCategories(userId: number) {
+            this.categories = []
+            this.expenses = []
             const {findOne} = useStrapi()
 
             const response = await findOne("users", userId, {
-                populate: "categories"
+                populate: {
+                    categories: {
+                        populate: ["expenses"]
+                    },
+                }
             })
 
-            this.categories = response.categories
+            for (const category of response.categories) {
+                category.createdAt = new Date(category.createdAt).toLocaleDateString("en-GB")
+                this.expenses.push(...this.getCategoryExpenses(category))
+                this.categories.push(category)
+            }
         },
-        async createOrUpdateCategory(type: string, payload: ICategory) {
+        getCategoryExpenses(category: ICategory) {
+            return (category.expenses || []).map(expense => ({
+                ...expense,
+                category: category.title,
+                createdAt: new Date(expense.createdAt).toLocaleDateString("en-GB")
+            }))
+        },
+        async initCategory(category: ICategory, type: string) {
             const {create, update} = useStrapi()
+            const authStore = useAuthStore()
+            const user = authStore.user
             switch (type) {
                 case "create":
-                    await create('categories', {title: payload.title, budgetLimit: payload.limit, user: payload.userId})
-                    // this.categories.push(payload)
+                    await create("categories", {title: category.title, budgetLimit: category.budgetLimit, user: user.id})
+                    await this.fetchUserCategories(user.id)
                     break
                 case "update":
-                    await update('categories', payload.id, {title: payload.title, budgetLimit: payload.limit})
-                    // this.categories.push(payload)
+                    await update("categories", category.id, {title: category.title, budgetLimit: category.budgetLimit, user: user.id})
+                    await this.fetchUserCategories(user.id)
                     break
             }
         },
-        async deleteCategory(categoryId: number) {
+        async deleteCategory(category: ICategory) {
+            const authStore = useAuthStore()
+            const user = authStore.user
             const { delete: _delete } = useStrapi()
-            await _delete('categories', categoryId)
-        }
+            await _delete("categories", category.id)
+            await this.fetchUserCategories(user.id)
+        },
+        async initExpense(expense: IExpense, type: string) {
+            const {create, update} = useStrapi()
+            const authStore = useAuthStore()
+            const user = authStore.user
+            switch (type) {
+                case "create":
+                    await create("expenses", {amount: expense.amount, categories: expense.categoryId})
+                    await this.fetchUserCategories(user.id)
+                    break
+                case "update":
+                    await update("expenses", expense.id, {amount: expense.amount, categories: expense.categoryId})
+                    await this.fetchUserCategories(user.id)
+                    break
+            }
+        },
+        async deleteExpense(expense: IExpense) {
+            const authStore = useAuthStore()
+            const user = authStore.user
+            const { delete: _delete } = useStrapi()
+            await _delete("expenses", expense.id)
+            await this.fetchUserCategories(user.id)
+        },
     },
 })
