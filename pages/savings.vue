1430605afa04eda68a19a8971c031684261c7312
GentIeman <ilyashepelev23@gmail.com>
2023-12-22 15:14:00 +0300

    init

---
+++ pages/savings.vue
@@ -0,0 +1,14 @@
+<template>
+  <AppHeader
+    title="Savings"
+    subtitle="Set goals for the future"
+  />
+</template>
+
+<script setup lang="ts">
+import AppHeader from "~/components/AppHeader.vue"
+</script>
+
+<style scoped>
+
+</style>
