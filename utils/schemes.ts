524936768834059a9bf7dad6e5ebc02433fca080
GentIeman <ilyashepelev23@gmail.com>
2023-12-22 23:24:36 +0300

    feat: goal form validation schema

--- utils/schemes.ts
+++ utils/schemes.ts
@@ -1,4 +1,4 @@
-import {object, string, number} from "yup"
+import {object, string, number, date} from "yup"
 
 export const registerFormValidation = object({
     email: string().email("You must enter an email").required("Required"),
@@ -20,5 +20,12 @@ export const categoryFormValidation = object({
 
 export const expenseFormValidation = object({
     category: string(),
-    amount: number().transform((value) => Number.isNaN(value) ? null : value ).nullable().moreThan(1, "Upps...").max(99999999, "Do you have that much money ?").required("Required")
+    amount: number().transform((value) => Number.isNaN(value) ? null : value ).nullable().moreThan(1, "Upps...").max(99999999, "Do you have that much money?").required("Required")
+})
+
+export const goalFormValidation = object({
+    title: string().min(3, "Must be at least 3 characters").required("Required"),
+    currentAmount: number().transform((value) => Number.isNaN(value) ? null : value ).nullable().moreThan(1, "Upps...").max(99999999, "Do you have that much money?"),
+    totalAmount: number().transform((value) => Number.isNaN(value) ? null : value ).nullable().moreThan(1, "Upps...").max(99999999, "Do you want how much money?").required("Required"),
+    endDate: date().required("Required").min(new Date().toISOString().slice(0, 10), "Date is too early").max(new Date(new Date().setFullYear(new Date().getFullYear() + 4)).toISOString().slice(0, 10), "Future").nullable().typeError("Invalid Date")
 })
