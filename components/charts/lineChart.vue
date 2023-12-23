19e25e557bb5224678fb99176c8f00f65f6c4a59
GentIeman <ilyashepelev23@gmail.com>
2023-12-23 22:44:51 +0300

    init

---
+++ components/charts/lineChart.vue
@@ -0,0 +1,55 @@
+<template>
+  <Line
+    :data="data"
+    :options="computedOptions"
+  />
+</template>
+
+<script setup lang="ts">
+import {Line} from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const props = defineProps({
+  datasets: {
+    required: true
+  },
+  labels: {
+    required: true
+  },
+  options: {
+    required: true
+  }
+})
+
+const data = computed(() => {
+  return {
+    labels: props.labels,
+    datasets: props.datasets,
+  }
+})
+
+const computedOptions = computed(() => props.options)
+</script>
+
+<style scoped>
+
+</style>
