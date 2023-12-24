5cde771b1d6033e739ff669cda93fdea22c33001
GentIeman <ilyashepelev23@gmail.com>
2023-12-24 20:37:17 +0300

    feat: status column

--- components/AppTable.vue
+++ components/AppTable.vue
@@ -14,6 +14,9 @@
         />
       </UDropdown>
     </template>
+    <template #status-data="{ row }">
+      {{ row.status.value }}
+    </template>
   </UTable>
   <div
     class="flex justify-end px-3 py-3.5 border-t border-gray-200 dark:border-gray-700"
