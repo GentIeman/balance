a52779fe2a0f8b03f9a1a8e6b6d6ca2736af59c5
GentIeman <ilyashepelev23@gmail.com>
2023-12-25 13:30:57 +0300

    init

---
+++ pages/pension.vue
@@ -0,0 +1,15 @@
+<template>
+  <AppHeader
+    title="Pension (beta)"
+    subtitle="It is worth thinking about pension today"
+  />
+</template>
+
+<script setup lang="ts">
+
+import AppHeader from "~/components/AppHeader.vue"
+</script>
+
+<style scoped>
+
+</style>
