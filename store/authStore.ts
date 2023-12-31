e8c29a688e8e18194165532132462982750f9882
GentIeman <ilyashepelev23@gmail.com>
2023-12-31 21:25:59 +0300

    style: code formatting

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,40 +1,35 @@
-import { defineStore } from "pinia"
-import type {IAuthState} from "~/utils/interfaces"
-import {useBalanceStore} from "~/store/balanceStore"
+import {defineStore} from "pinia"
+import type {IAuthState, IUser} from "~/utils/interfaces"
 
 export const useAuthStore = defineStore("authStore", {
     state: (): IAuthState => ({
         user: {},
-        errors: null
     }),
     actions: {
-        async auth(type: string, payload: object | null) {
-            const {login, register, logout} = useStrapiAuth()
-            this.errors = null
-            try {
-                switch (type) {
-                    case "login" :
-                        await login({identifier: payload.email, password: payload.password})
-                        this.user = useStrapiUser()
-                        break
-                    case "register":
-                        await register({email: payload.email, password: payload.password, username: payload.username, salary: payload.salary, pensionYear: new Date(`${payload.pensionYear}`)})
-                        this.user = useStrapiUser()
-                        break
-                    case "logout":
-                        const balanceStore = useBalanceStore()
-                        logout()
-                        balanceStore.clearCache()
-                        this.user = {}
-                        break
-                }
-            } catch (err: any) {
-                this.errors = err.error
-            }
+        async login(payload: IUser) {
+            const {login} = useStrapiAuth()
+            await login({
+                identifier: payload.email,
+                password: payload.password
+            })
+            this.user = useStrapiUser()
         },
-        cacheUser() {
-            const user = useStrapiUser()
-            if (user) this.user = user
-        }
+        async register(payload: IUser) {
+            const {register} = useStrapiAuth()
+            await register({
+                    email: payload.email,
+                    password: payload.password,
+                    username: payload.username,
+                    // @ts-ignore
+                    salary: payload.salary,
+                    // @ts-ignore
+                    pensionYear: new Date(payload.pensionYear)
+                })
+            this.user = useStrapiUser()
+        },
+        logout() {
+            const {logout} = useStrapiAuth()
+            logout()
+        },
     },
 })
