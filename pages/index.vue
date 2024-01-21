cd525533a05bcff709e353a606a0c56e9e26105f
GentIeman <ilyashepelev23@gmail.com>
2024-01-21 19:14:01 +0300

    style: code formatted

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,31 @@
 <template>
   <UContainer
-    class="w-full sm:p-0 lg:p-0"
-    v-if="categories.length == 0"
+    class="w-full"
+    v-if="getCategoriesCount == 0"
   >
     <UCard class="flex flex-col w-full justify-center">
       <template #header>
-        <h2 class="flex flex-col text-zinc-800 text-[30px] font-semibold">
-          Hello 👋
-          <span class="text-zinc-600 text-lg font-normal">You don't have any categories yet</span>
-        </h2>
+        <header class="flex">
+          <h2 class=" flex flex-col text-zinc-800 text-[30px] font-semibold">
+            Hello 👋
+            <span class="text-zinc-600 text-lg font-normal">You don't have any categories yet</span>
+          </h2>
+        </header>
       </template>
       <UButton
         block
-        @click="isOpen = true"
+        @click="isShowCategoryForm = true"
       >
         Create category
       </UButton>
-    </Ucard>
+    </UCard>
     <UModal
       prevent-close
-      v-model="isOpen"
+      v-model="isShowCategoryForm"
     >
       <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
         <template #header>
-          <div class="flex items-center justify-between">
+          <header class="flex items-center justify-between">
             <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
               Create category
             </h3>
@@ -32,43 +34,47 @@
               variant="ghost"
               icon="i-heroicons-x-mark-20-solid"
               class="-my-1"
-              @click="isOpen = false"
+              @click="isShowCategoryForm = false"
             />
-          </div>
+          </header>
         </template>
         <CategoryForm
-          @close-modal="isOpen = false"
-          :category="category"
+          @close-modal="isShowCategoryForm = false"
+          :category="{}"
         />
       </UCard>
     </UModal>
   </UContainer>
-  <div
+  <UContainer
     v-else
-    class="grid gap-10"
+    class="grid gap-10 w-full"
   >
     <UContainer class="flex justify-center w-full sm:p-5 bg-red-500 rounded-lg">
       <p class="text-white text-base font-semibold">
         Financial applications pacify spontaneous spending, help to save and multiply the accumulated
       </p>
     </UContainer>
-    <div class="grid grid-cols-3 gap-10">
-      <UCard :class="{'col-span-2': expenses.length > 0, 'col-span-full': expenses.length == 0}">
+    <UContainer class="grid grid-cols-3 gap-10 w-full sm:p-0 lg:p-0">
+      <UCard :class="{'col-span-2': getExpensesCount > 0, 'col-span-full': getExpensesCount == 0}">
         <template #header>
-          <div class="flex justify-between">
+          <header class="flex justify-between">
             <h2 class="text-zinc-800 text-base font-semibold leading-tight">
               Categories Status
             </h2>
-          </div>
+          </header>
         </template>
         <AppTable
-          :payload="categoriesWithStatus"
+          :rows="getCategoryStatus"
           :page-count="5"
-          :columns="categoriesStatusTableColumns"
+          :columns="[
+            {key: 'title', label: 'Title', sortable: true},
+            {key: 'budgetLimit', label: 'Limit', sortable: true},
+            {key: 'status', label: 'Status', sortable: true}
+          ]" 
         />
       </UCard>
       <UCard
-        v-if="expenses.length > 0"
+        v-if="getExpensesCount > 0"
         class="col-span-1"
         :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }"
       >
@@ -77,128 +83,71 @@
             Categories-wise Overview
           </h3>
         </template>
-        <UContainer class="grid place-items-center h-full">
-          <Pie
-            :data="categoriesChartData"
-            :options="chartOptions"
+        <UContainer class="grid place-items-center h-full w-full">
+          <PieChart
+            :data="getTotalExpensesByCategory()"
+            :config="PieChartConfig"
+            :options="{maintainAspectRatio: false}"
           />
         </UContainer>
       </UCard>
-    </div>
-    <div class="grid grid-cols-3 auto-rows-max gap-10">
+    </UContainer>
+    <UContainer class="grid gap-10 w-full sm:p-0 lg:p-0">
       <UCard
-        v-if="topThreeSavings.length > 0"
-        class="col-span-3 max-h-[450px]"
-        :class="{'col-span-1': expenses.length > 0}"
-      >
-        <template #header>
-          <header class="flex items-center justify-between">
-            <h3 class="text-zinc-800 text-lg font-semibold">
-              Savings
-            </h3>
-          </header>
-        </template>
-        <ul class="flex flex-col gap-10">
-          <li
-            class="w-full"
-            v-for="saving in topThreeSavings"
-            :key="saving.id"
-          >
-            <p class="text-cyan-950 text-lg font-semibold">
-              {{ saving.title }}
-            </p>
-            <UProgress
-              indicator
-              :value="calcProgress(saving.currentAmount, saving.totalAmount)"
-            />
-            <p class="text-black text-opacity-80 text-xs font-normal">
-              {{ saving.currentAmount ?? 0 }} / {{ saving.totalAmount }}
-            </p>
-          </li>
-        </ul>
-      </UCard>
-      <UCard
-        v-if="expenses.length > 0"
-        class="grid col-span-2 max-h-[450px] bg-white"
+        v-if="getExpensesCount > 0"
+        class="grid col-span-2 bg-white"
         :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }"
       >
         <UContainer class="h-full">
-          <Bar
-            :data="expensesChartData"
-            :options="chartOptions"
+          <TimeLineBarChart
+            locales="en-US"
+            :locale-date-options="{ year: 'numeric', month: 'long' }"
+            :data="expenses"
+            :options="{maintainAspectRatio: false, responsive: true}"
+            interval="month"
+            :config="timeLineBarConfig"
           />
         </UContainer>
       </UCard>
-    </div>
-  </div>
+    </UContainer>
+  </UContainer>
 </template>
 
 <script setup lang="ts">
 import {UButton, UCard, UContainer, UModal, UProgress} from "#components"
-import AppHeader from "~/components/AppHeader.vue"
 import CategoryForm from "~/components/forms/categoryForm.vue"
-import {useBalanceStore} from "~/store/balanceStore"
-import type {ICategory} from "~/utils/interfaces"
-import {useAuthStore} from "~/store/authStore"
-import {Bar, Pie} from "vue-chartjs"
-import {generateMonthBar, generatePieChart} from "~/utils/chartUtils"
-import {calcProgress} from "~/utils/tools"
-import {categoriesStatusTableColumns} from "~/utils/tableSchemes"
 import AppTable from "~/components/AppTable.vue"
+import {useExpenseStore} from "~/store/expenseStore"
+import TimeLineBarChart from "~/components/charts/timeLineBarChart.vue"
+import PieChart from "~/components/charts/pieChart.vue"
+import type {IBarChartConfig} from "~/utils/interfaces"
 
-const balanceStore = useBalanceStore()
-const authStore = useAuthStore()
-const user = authStore.user
-const isOpen = ref<boolean>(false)
+const expenseStore = useExpenseStore()
+const {
+  expenses,
+  getExpensesCount,
+  getTotalExpensesByCategory,
+  getCategoryStatus,
+  getCategoriesCount
+} = storeToRefs(expenseStore)
+const {fetchUserExpenses} = expenseStore
 
-const topThreeSavings = computed(() => balanceStore.getSortedSavingsByPercent.slice(0, 3))
-const categories = computed(() => balanceStore.getCategories())
-const totalExpenses = computed(() => balanceStore.getTotalCategoryExpenses())
-const expenses = computed(() => balanceStore.getCategoriesExpenses)
+const isShowCategoryForm = ref<boolean>(false)
 
-const setCategoriesStatus = (categories: object[]) => {
-  return categories.map(category => ({
-    ...category,
-    status: balanceStore.getTotalCategoryExpenses(category) > category.budgetLimit
-        ? {value: "Warn", class: "text-red-500"}
-        : {value: "Good", class: "text-green-500"}
-  }))
-}
-
-const categoriesWithStatus = computed(() => setCategoriesStatus(categories.value))
-
-const categoriesChartConfig = {
+const PieChartConfig = {
   label: "title",
   dataKey: "totalExpenses",
-  colorKey: "color"
+  backgroundColor: "color"
 }
 
-const expensesChartConfig = {
-  structure: {
-    date: "createdAt",
-    content: "amount"
-  },
+const timeLineBarConfig = {
+  structure: {dateKey: "createdAt", contentKey: "amount"},
   months: 12,
   chartLabel: "Expenses by Month",
   backgroundColor: "cyan"
 }
 
-const categoriesChartData = computed(() => generatePieChart(totalExpenses.value, categoriesChartConfig))
-const expensesChartData = computed(() => generateMonthBar(expenses.value, expensesChartConfig))
-
-const chartOptions = ref({
-  maintainAspectRatio: false,
-})
-
-const category = reactive<ICategory>({
-  id: 0,
-  title: "",
-  budgetLimit: 10,
-})
-
-definePageMeta({
-  middleware: ["auth"]
-})
+definePageMeta({ middleware: ["auth"] })
 
 useSeoMeta({
   title: "Dashboard",
@@ -206,8 +155,7 @@ useSeoMeta({
 })
 
 onBeforeMount(async () => {
-  if (balanceStore.categories.length == 0) await balanceStore.fetchUserCategories(user.id)
-  if (balanceStore.savings.length == 0) await balanceStore.fetchUserSavings(user.id)
+  await fetchUserExpenses()
 })
 
 </script>
