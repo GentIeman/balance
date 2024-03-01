50ed53bc7bb101d00288e407b71b064a1e159aa4
GentIeman <ilyashepelev23@gmail.com>
2024-03-01 19:54:22 +0300

    init

---
+++ components/AppLoader.vue
@@ -0,0 +1,21 @@
+<template>
+  <UContainer class="grid place-content-center w-full h-screen absolute top-0 left-0 bg-white z-10 max-w-none">
+    <UIcon
+      :name="props.icon"
+      class="bg-cyan-500 motion-safe:animate-spin"
+      :style="{'font-size': props.size}"
+    />
+  </UContainer>
+</template>
+
+<script setup lang="ts">
+import {UContainer, UIcon} from "#components"
+const props = defineProps<{
+  icon: string,
+  size: string
+}>()
+</script>
+
+<style scoped>
+
+</style>
