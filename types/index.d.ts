25012213d18ec1668054f85660fc9697e33a8409
GentIeman <ilyashepelev23@gmail.com>
2024-03-03 13:49:15 +0300

    init

---
+++ types/index.d.ts
@@ -0,0 +1,53 @@
+declare global {
+    interface IUser {
+        id?: number
+        email: string,
+        username?: string,
+        password: string,
+        salary?: number,
+        pensionYear?: number
+    }
+
+    interface ICategory {
+        id?: number,
+        title: string,
+        budgetLimit: number,
+        totalExpenses?: number
+    }
+
+    interface IExpense {
+        id?: number,
+        amount: number,
+        createdAt: string,
+        categories?: {
+            id: number
+            title: string
+        }
+    }
+
+    interface IChart {
+        labels: string[];
+        datasets: {
+            label?: string;
+            backgroundColor: string | string[];
+            data: number[];
+        }[];
+    }
+
+    interface IBarChartConfig {
+        structure: {
+            dateKey: string,
+            contentKey: string
+        },
+        days?: number,
+        months?: number,
+        label: string,
+        backgroundColor: string
+    }
+
+    interface IPieChartConfig {
+        label: string,
+        dataKey: string,
+        colorKey: string
+    }
+}
