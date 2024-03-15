61e3c33186bb98167745ef7bab244955bcc5e535
GentIeman <ilyashepelev23@gmail.com>
2024-03-15 12:06:31 +0300

    init

---
+++ components/AppDropDown.vue
@@ -0,0 +1,44 @@
+<template>
+  <UDropdown :items="actions(props.items)">
+    <UButton
+      color="gray"
+      variant="ghost"
+      icon="i-heroicons-ellipsis-horizontal-20-solid"
+    />
+  </UDropdown>
+</template>
+
+<script setup lang="ts">
+import {UButton, UDropdown} from "#components"
+
+const props = defineProps<{
+  items: object,
+  actionLabels: string[]
+}>()
+
+const emits = defineEmits<{
+  click: [value: object],
+}>()
+
+const actions = (item: object) => {
+  const allActions = [
+    {
+      label: 'Edit',
+      icon: 'i-heroicons-pencil-square-20-solid',
+      click: () => emits("click", item)
+    },
+    {
+      label: 'Delete',
+      icon: 'i-heroicons-trash-20-solid',
+      click: () => emits("click", item)
+    },
+    {
+      label: 'Clear limit',
+      icon: 'i-heroicons-trash-20-solid',
+      click: () => emits("click", item)
+    }
+  ]
+
+  return [allActions.filter(action => props.actionLabels.includes(action.label.toLowerCase()))]
+}
+</script>
