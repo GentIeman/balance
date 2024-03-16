05f2a61553c29d3e007cdf1783d0512046017671
GentIeman <ilyashepelev23@gmail.com>
2024-03-16 15:03:35 +0300

    update: period of years

--- components/AppForm.vue
+++ components/AppForm.vue
@@ -48,7 +48,7 @@
         @input="$event.target.value = $event.target.value == '' && input.type == 'number' ? 1 : $event.target.value "
         :placeholder="input.placeholder"
         :min="input.type == 'date' ? new Date().toISOString().split('T')[0] : ''"
-        :max="input.type == 'date' ? new Date(new Date().setFullYear(new Date().getFullYear() + 60)).toISOString().split('T')[0] : ''"
+        :max="input.type == 'date' ? new Date(new Date().setFullYear(new Date().getFullYear() + 3)).toISOString().split('T')[0] : ''"
         :ui="{ icon: { trailing: { pointer: '' } } }"
       >
         <template
